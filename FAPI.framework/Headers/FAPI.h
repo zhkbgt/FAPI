1fbef265287ff65a1e95f34712f617e34d5c361b
zh神夕 <kkbbggtt@163.com>
2019-04-03 04:16:18 +0800

    0.1.0

---
+++ FAPI.framework/Headers/FAPI.h
@@ -0,0 +1,34 @@
+//
+//  FAPI.h
+//  FAPI
+//
+//  Created by 张兵 on 2019/4/3.
+//  Copyright © 2019 张兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FAPI.
+FOUNDATION_EXPORT double FAPIVersionNumber;
+
+//! Project version string for FAPI.
+FOUNDATION_EXPORT const unsigned char FAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FAPI/PublicHeader.h>
+
+#import <FAPI/APIWindowContainer.h>
+#import <FAPI/APIWidgetContainer.h>
+#import <FAPI/APIManager.h>
+#import <FAPI/APIEvent.h>
+#import <FAPI/APIUpdateManager.h>
+#import <FAPI/APIWebView.h>
+#import <FAPI/APIScriptMessage.h>
+#import <FAPI/APIModuleMethod.h>
+
+#import <FAPI/UZAppDelegate.h>
+#import <FAPI/UZWidget.h>
+#import <FAPI/UZAppUtils.h>
+#import <FAPI/UZModule.h>
+#import <FAPI/APIWebView.h>
+#import <FAPI/NSDictionaryUtils.h>
+#import <FAPI/UZModule.h>
